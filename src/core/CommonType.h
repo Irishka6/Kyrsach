24f9ebdc0864d272f9279f4941bb737f73a79aae
vikaar68 <vikaarkashina3215@gmail.com>
2025-11-25 16:38:22 +0300

    Авторизация(Вход)

---
+++ src/core/CommonType.h
@@ -0,0 +1,9 @@
+#ifndef COMMONTYPES_H
+#define COMMONTYPES_H
+
+// Предварительные объявления чтобы разорвать циклические зависимости
+class Project;
+class Developer;
+class Tasks;
+
+#endif // COMMONTYPES_H
